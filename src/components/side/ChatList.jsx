6a3aa8dc5a353c184dc275a61179815b72db8bef
llvovll89 <svvvs5579@naver.com>
2023-03-05 22:57:56 +0900

    chatList - hegit 값 수정 + 전체보완

--- src/components/side/ChatList.jsx
+++ src/components/side/ChatList.jsx
@@ -3,14 +3,15 @@ import { doc, onSnapshot } from 'firebase/firestore'
 import { db } from '../../firebase';
 import {ResisterContext} from '../../context/ResisterContext'
 import { ChatContext } from '../../context/ChatContext'
-import './Side.css'
 import { useWrapContext } from '../../context/WrapContext';
+import './Side.css'
 
 const ChatList = () => {
   const [ chatList, setChatList ] = useState([]);
   const { currentUser } = useContext(ResisterContext)
   const { dispatch } = useContext(ChatContext)
-  const { dark } = useWrapContext();
+  const { dark , modalMode , logModal } = useWrapContext();
+
 
   useEffect(() => {
     const getChatList = () => {
@@ -30,17 +31,21 @@ const ChatList = () => {
     dispatch({type: "CHANGE_USER", payload: userinfo})
   }
 
+  const userClick = () => {
+    return modalMode();
+  }
+
   return (
     <>
-    <div className="chat_list" style={{color: dark ? "#FFFFFF" : "#181818"}}>
+    <div className="chat_list" style={{ background: !logModal ? "#FFF" : "#B2B2B2"}}>
       {Object.entries(chatList)?.sort((a,b) => b[1].date - a[1].date).map((chat) => (
         <div className="user_list"  
         style={{boxShadow : dark ? "1px 1px 2px rgba(255,255,255,0.12)" : "1px 1px 2px rgba(0,0,0,0.12)"}}
-        key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)}>
+        key={chat[0]} onClick={() => { handleSelect(chat[1].userInfo); userClick(); }}>
         <img src={chat[1].userInfo.photoURL} alt="" />
         <div className="list_chat">
             <span>{chat[1].userInfo.displayName}</span>
-                <p>{chat[1].lastMessage?.text}</p>
+                <p className='list_check_user'>{chat[1].lastMessage.text.length > 30 ? chat[1].lastMessage.text.substr(0, 30) + '...' : chat[1].lastMessage.text}</p>
             </div>
             </div>
       ))}
