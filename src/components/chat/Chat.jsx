ddb6896ba028490e56ef4b20a26e453c971e2522
llvovll89 <svvvs5579@naver.com>
2023-03-05 22:56:43 +0900

    디자인 + contextAPI 적용

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -8,25 +8,29 @@ import MsgContainer from './MsgContainer';
 import ChatInput from './ChatInput';
 
 // icons
-import {BsCameraVideo , BsSun} from 'react-icons/bs'
+import {BsSun} from 'react-icons/bs'
 import { AiOutlineUserAdd } from 'react-icons/ai'
+import {SlSizeFullscreen } from 'react-icons/sl'
 import {CiSettings} from 'react-icons/ci'
 import {MdDarkMode} from 'react-icons/md';
 import { useContext } from 'react';
 import { ChatContext } from '../../context/ChatContext';
 
-
 const Chat = () => {
   const { data } = useContext(ChatContext);
-  const { darkMode , dark } = useWrapContext();
-  
+  const { darkMode , dark , modalMode, modalOn} = useWrapContext();
+
+  const clickHandler = () => {
+    modalMode(modalOn);
+  }
+
   return (
-    <div className='chat'>
-    <div className="chat_header">
+    <div className={modalOn ? "chat mobile" : "chat" }>
+    <div className="chat_header" style={{ background : dark ? "#181818" : "#ffff" , color: dark? "#fff" : "#181818"}}>
   {/* 아래 하단 span태그는 채팅창 - 채팅리스트 클릭(사람이름) */ }
-    <span>{data.user?.displayName}</span>
+    <span className='user_name'>{data.user.displayName}</span>
     <div className="chat_header_icons">
-    <span><BsCameraVideo /></span>
+    <span onClick={clickHandler}><SlSizeFullscreen /></span>
     <span><AiOutlineUserAdd /></span>
     <span><CiSettings /></span>
     <span onClick={darkMode}>{dark ? <BsSun /> : <MdDarkMode/> }</span>
